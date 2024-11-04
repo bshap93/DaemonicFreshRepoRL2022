5f3a42364c7fc6a6b878ae94015c86f22f69eeee
Ben Shapiro <bshap93@gmail.com>
2024-11-03 17:54:45 -0700

    Importing spiders

--- Assets/Project/Gameplay/ItemManagement/ItemUseAbilities/CharacterHandleShield.cs
+++ Assets/Project/Gameplay/ItemManagement/ItemUseAbilities/CharacterHandleShield.cs
@@ -5,51 +5,37 @@ using UnityEngine;
 
 namespace Project.Gameplay.ItemManagement.ItemUseAbilities
 {
-    [AddComponentMenu("TopDown Engine/Character/Abilities/Character Handle Shield")]
     public class CharacterHandleShield : CharacterAbility
     {
-        [MMInspectorGroup("Input", true, 11)]
-        /// if this is true, this shield will be able to read input, otherwise input will be disabled
-        [Tooltip("if this is true, this shield will be able to read input, otherwise input will be disabled")]
-        public bool InputAuthorized = true;
-        /// if true, the shield button will need to be released to block again
-        [Tooltip("if true, the shield button will need to be released to block again")]
-        public bool ContinuousPress = true;
-
         [MMInspectorGroup("Shield", true, 10)] public Shield InitialShield;
         public Transform ShieldAttachment;
         public bool AutomaticallyBindAnimator = true;
-
-        /// the ID of this CharacterHandleShield. This will be used to determine what handle shield ability should equip a shield.
-        [Tooltip("ID of this shield handler - used to determine which handler equips which shield")]
         public int HandleShieldID = 1;
 
-        [MMInspectorGroup("Input", true, 11)] [Tooltip("The name of the input to use for shield activation")]
-        public string ShieldButtonName = "Shield";
+        [MMInspectorGroup("Input", true, 11)] public bool InputAuthorized = true;
+        public bool ContinuousPress = true;
 
         protected MMInput.IMButton _shieldButton;
 
-
-        protected bool _shieldButtonPressed = false;
-
         public Shield CurrentShield { get; protected set; }
         public Animator CharacterAnimator { get; protected set; }
-        public override string HelpBoxText()
-        {
-            return
-                "This component handles shield mechanics. You can set an initial shield and configure shield-specific parameters here.";
-        }
 
         protected override void Initialization()
         {
             base.Initialization();
+
+            // Initialize input button
+            if (_inputManager != null)
+                _shieldButton = new MMInput.IMButton(
+                    _character.PlayerID, "Shield", ShieldButtonDown, ShieldButtonPressed, ShieldButtonUp);
+
+            // Setup shield and animator
+            CharacterAnimator = _animator;
             SetupShield();
         }
 
         protected virtual void SetupShield()
         {
-            CharacterAnimator = _animator;
-
             if (ShieldAttachment == null) ShieldAttachment = transform;
 
             if (InitialShield != null) EquipShield(InitialShield);
@@ -57,42 +43,63 @@ namespace Project.Gameplay.ItemManagement.ItemUseAbilities
 
         protected override void HandleInput()
         {
-            if (!AbilityAuthorized || !InputAuthorized || CurrentShield == null) return;
+            // Early exit if conditions aren't met
+            if (!AbilityAuthorized
+                || !InputAuthorized
+                || CurrentShield == null
+                || _inputManager == null
+                || _shieldButton == null)
+                return;
 
-            if (_inputManager.ShieldInput.State.CurrentState == MMInput.ButtonStates.ButtonDown) ShieldStart();
+            if (_shieldButton.State.CurrentState == MMInput.ButtonStates.ButtonDown) ShieldStart();
 
-            if (_inputManager.ShieldInput.State.CurrentState == MMInput.ButtonStates.ButtonUp) ShieldStop();
+            if (_shieldButton.State.CurrentState == MMInput.ButtonStates.ButtonUp) ShieldStop();
 
-            if (ContinuousPress && _inputManager.ShieldInput.State.CurrentState == MMInput.ButtonStates.ButtonPressed)
+            if (ContinuousPress && _shieldButton.State.CurrentState == MMInput.ButtonStates.ButtonPressed)
                 CurrentShield.RaiseShield();
         }
 
         public virtual void ShieldStart()
         {
-            if (CurrentShield != null)
-            {
-                PlayAbilityStartFeedbacks();
-                CurrentShield.RaiseShield();
-            }
+            if (CurrentShield == null) return;
+
+            PlayAbilityStartFeedbacks();
+            CurrentShield.RaiseShield();
         }
 
         public virtual void ShieldStop()
         {
-            if (CurrentShield != null)
-            {
-                PlayAbilityStopFeedbacks();
-                CurrentShield.LowerShield();
-            }
+            if (CurrentShield == null) return;
+
+            PlayAbilityStopFeedbacks();
+            CurrentShield.LowerShield();
+        }
+
+        protected virtual void ShieldButtonDown()
+        {
+            if (_shieldButton != null) _shieldButton.State.ChangeState(MMInput.ButtonStates.ButtonDown);
+        }
+        protected virtual void ShieldButtonPressed()
+        {
+            if (_shieldButton != null) _shieldButton.State.ChangeState(MMInput.ButtonStates.ButtonPressed);
+        }
+        protected virtual void ShieldButtonUp()
+        {
+            if (_shieldButton != null) _shieldButton.State.ChangeState(MMInput.ButtonStates.ButtonUp);
         }
 
         public virtual void EquipShield(Shield newShield)
         {
             if (CurrentShield != null) Destroy(CurrentShield.gameObject);
 
-            CurrentShield = Instantiate(newShield, ShieldAttachment.position, ShieldAttachment.rotation);
-            CurrentShield.transform.parent = ShieldAttachment;
-            CurrentShield.SetOwner(_character, this);
-            CurrentShield.Initialization();
+            if (newShield != null)
+            {
+                CurrentShield = Instantiate(newShield, ShieldAttachment.position, ShieldAttachment.rotation);
+                CurrentShield.transform.parent = ShieldAttachment;
+                CurrentShield.SetOwner(_character, this);
+                CurrentShield.Initialization();
+                Debug.Log($"Shield equipped successfully: {CurrentShield.name}");
+            }
         }
 
         protected override void OnDeath()
---
+++ Assets/Project/Tests/ShieldDebugTests.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using MoreMountains.TopDownEngine;
+using Project.Gameplay.ItemManagement.ItemUseAbilities;
+using TopDownEngine.Common.Scripts.Characters.Core;
+
+namespace Project.Tests
+{
+    public class ShieldDebugTest : MonoBehaviour
+    {
+        protected CharacterHandleShield _handleShield;
+        protected InputManager _inputManager;
+
+        void Start()
+        {
+            StartCoroutine(WaitAndTest());
+        }
+
+        System.Collections.IEnumerator WaitAndTest()
+        {
+            // Wait for player spawn
+            yield return new WaitForSeconds(0.5f);
+
+            var player = FindObjectOfType<Character>();
+            if (player != null)
+            {
+                _handleShield = player.GetComponent<CharacterHandleShield>();
+                Debug.Log($"Found CharacterHandleShield: {_handleShield != null}");
+                if (_handleShield != null)
+                {
+                    Debug.Log($"Shield Equipment State:");
+                    Debug.Log($"- Initial Shield assigned: {_handleShield.InitialShield != null}");
+                    Debug.Log($"- Current Shield exists: {_handleShield.CurrentShield != null}");
+                    Debug.Log($"- Shield Attachment exists: {_handleShield.ShieldAttachment != null}");
+                }
+            }
+            else
+            {
+                Debug.LogError("No player character found!");
+            }
+
+            _inputManager = InputManager.Instance;
+            Debug.Log($"Input Manager exists: {_inputManager != null}");
+        }
+
+        void OnGUI()
+        {
+            if (_handleShield != null && _handleShield.CurrentShield != null)
+            {
+                GUI.Label(new Rect(10, 10, 300, 20), 
+                    $"Shield State: {_handleShield.CurrentShield.ShieldState?.CurrentState}");
+            }
+        }
+    }
+}
