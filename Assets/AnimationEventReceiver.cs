fd2f16c7ae0f30e0ebe1118200f3ffa79a47b7c5
Ben Shapiro <bshap93@gmail.com>
2024-11-06 16:00:34 -0700

    Have 2 working enemies

---
+++ Assets/AnimationEventReceiver.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AnimationEventReceiver : MonoBehaviour
+{
+    // Empty method just to catch the animation event
+    public void PlayAudio()
+    {
+        // Optionally log or add actual audio functionality later
+        // Debug.Log("Audio event received");
+    }
+}
