a8161dec6076b4af32690cd15ae21b2fa34bdd53
Ben Shapiro <bshap93@gmail.com>
2024-11-18 09:14:51 -0700

    Working crossbow

--- Assets/Project/Gameplay/Magic/MagicResourceUI.cs
+++ Assets/Project/Gameplay/Magic/MagicResourceUI.cs
@@ -7,32 +7,67 @@ namespace Project.Gameplay.Magic
     public class MagicResourceUI : MonoBehaviour
     {
         public MMProgressBar HealthBar;
-        public MMProgressBar KinemaBar;
-        public MMProgressBar FavourBar;
+        public MMProgressBar PrimaryBar;
+        public MMProgressBar SecondaryBar;
         [SerializeField] MagicSystem _magicSystem;
+
+        float _lastHealth;
+        float _lastPrimaryResource;
+        float _lastSecondaryResource;
+
         void Update()
         {
-            if (_magicSystem != null)
-            {
-                UpdateBar(
-                    HealthBar, _magicSystem.GetComponent<Health>().CurrentHealth,
-                    _magicSystem.GetComponent<Health>().MaximumHealth);
+            if (_magicSystem == null) return;
 
-                UpdateBar(KinemaBar, _magicSystem.Kinema.CurrentResource, _magicSystem.Kinema.MaxResource);
-                UpdateBar(FavourBar, _magicSystem.Favour.CurrentResource, _magicSystem.Favour.MaxResource);
+            // Check and update Health bar
+            var currentHealth = _magicSystem.GetComponent<Health>().CurrentHealth;
+            if (!Mathf.Approximately(currentHealth, _lastHealth))
+            {
+                _lastHealth = currentHealth;
+                UpdateBar(HealthBar, currentHealth, _magicSystem.GetComponent<Health>().MaximumHealth);
+            }
+
+            // Check and update Primary Resource bar
+            var currentPrimary = _magicSystem.PrimaryResource.CurrentResource;
+            if (!Mathf.Approximately(currentPrimary, _lastPrimaryResource))
+            {
+                _lastPrimaryResource = currentPrimary;
+                UpdateBar(PrimaryBar, currentPrimary, _magicSystem.PrimaryResource.MaxResource);
+            }
+
+            // Check and update Secondary Resource bar
+            var currentSecondary = _magicSystem.SecondaryResource.CurrentResource;
+            if (!Mathf.Approximately(currentSecondary, _lastSecondaryResource))
+            {
+                _lastSecondaryResource = currentSecondary;
+                UpdateBar(SecondaryBar, currentSecondary, _magicSystem.SecondaryResource.MaxResource);
             }
         }
 
-
         public void SetMagicSystem(MagicSystem magicSystem)
         {
-            Debug.Log("Magic system set.");
             _magicSystem = magicSystem;
+
+            // Initialize values to avoid unnecessary updates
+            _lastHealth = _magicSystem.GetComponent<Health>().CurrentHealth;
+            _lastPrimaryResource = _magicSystem.PrimaryResource.CurrentResource;
+            _lastSecondaryResource = _magicSystem.SecondaryResource.CurrentResource;
+
+            UpdateAllBars(); // Set initial values
         }
 
         void UpdateBar(MMProgressBar bar, float currentValue, float maxValue)
         {
             if (bar != null) bar.UpdateBar(currentValue, 0f, maxValue);
+        }
+
+        void UpdateAllBars()
+        {
+            if (_magicSystem == null) return;
+
+            UpdateBar(HealthBar, _lastHealth, _magicSystem.GetComponent<Health>().MaximumHealth);
+            UpdateBar(PrimaryBar, _lastPrimaryResource, _magicSystem.PrimaryResource.MaxResource);
+            UpdateBar(SecondaryBar, _lastSecondaryResource, _magicSystem.SecondaryResource.MaxResource);
         }
     }
 }
